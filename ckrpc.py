ed9ecf7fcc2b5c9b54f1c35c622b1b879e68c068
harpo <harpo@harpo.me>
2019-03-14 11:52:11 -0700

    crossbar/krpc integration test

---
+++ ckrpc.py
@@ -0,0 +1,29 @@
+import krpc
+from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
+from os import environ
+import asyncio
+from time import time
+
+class Ckerbal(ApplicationSession):
+    async def onJoin(self, details):
+        print('Ckerbal session started!')
+        self.conn = krpc.connect(name='george')
+        self.vessel = self.conn.space_center.active_vessel
+        self.refframe = self.vessel.orbit.body.reference_frame
+        def getPos():
+            with self.conn.stream(self.vessel.position, self.refframe) as p:
+                with p.condition:
+                    pos = p()
+                    print('new position', pos, time())
+                    self.publish(u'local.krpc.position', pos)
+        while True:
+            getPos()
+
+
+
+    async def onDisconnect(self):
+        asyncio.get_event_loop().stop()
+
+if __name__ == '__main__':
+    runner = ApplicationRunner(environ.get("AUTOBAHN_DEMO_ROUTER", u"ws://localhost:7777/ws"), u"realm1",)
+    runner.run(Ckerbal)
