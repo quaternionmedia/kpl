169c995f28685ea7e49e570d99d7e47f82e9779e
harpo <harpo@harpo.me>
2019-03-19 16:48:49 -0700

    added flight_chars

--- ckrpc.py
+++ ckrpc.py
@@ -3,27 +3,37 @@ from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
 from os import environ
 import asyncio
 from time import time
+from json import dumps
+flight_chars = ['aerodynamic_force', 'angle_of_attack',  'atmosphere_density', 'bedrock_altitude', 'center_of_mass', 'direction', 'drag', 'dynamic_pressure', 'elevation', 'equivalent_air_speed', 'g_force', 'heading', 'horizontal_speed', 'latitude', 'lift', 'longitude', 'mach', 'mean_altitude', 'pitch', 'roll', 'rotation', 'sideslip_angle', 'speed', 'speed_of_sound',  'static_air_temperature', 'static_pressure', 'static_pressure_at_msl', 'surface_altitude', 'terminal_velocity',  'total_air_temperature', 'true_air_speed', 'velocity', 'vertical_speed']
+# broken = ['ballistic_coefficient', 'drag_coefficient', 'lift_coefficient',  'reynolds_number','stall_fraction', 'thrust_specific_fuel_consumption','simulate_aerodynamic_force_at',]
+# redundant = ['normal', 'anti_normal', 'prograde', 'radial', 'retrograde', 'anti_radial']
 
 class Ckerbal(ApplicationSession):
     async def onJoin(self, details):
-        print('Ckerbal session started!')
+        print('kerbal session started!')
         self.conn = krpc.connect(name='george')
+        print('connected!')
         self.vessel = self.conn.space_center.active_vessel
         self.refframe = self.vessel.orbit.body.reference_frame
-        self.lpos = time()
-        def getPos():
-            with self.conn.stream(self.vessel.position, self.refframe) as p:
-                with p.condition:
-                    if time() - self.lpos > .1:
-                        self.lpos = time();
-                        pos = p()
-                        # print('new position', pos, time())
-                        self.publish(u'local.krpc.position', pos)
-
+        self.last_update = time()
+        def getFlightChars(x):
+            j = {}
+            for i in flight_chars:
+                j[i] = getattr(x, i)
+            # print('new attrs', j )
+            return j
+            # publish(u'local.krpc.position', j)
+        self.flightStats = self.conn.add_stream(self.vessel.flight, self.refframe)
+        self.flightStats.add_callback(getFlightChars)
+        self.flightStats.start()
         while True:
-            getPos()
-
-
+            with self.flightStats.condition:
+                if time() - self.last_update > 1:
+                    stats = getFlightChars(self.flightStats())
+                    print(stats)
+                    self.publish('local.krpc.flightStats',
+                                    dumps(stats))
+                    self.last_update = time()
 
     async def onDisconnect(self):
         asyncio.get_event_loop().stop()
