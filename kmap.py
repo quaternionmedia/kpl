2b4a831c601ed5b0a4e7299c890fe6c50691f1b0
harpo <harpo@harpo.me>
2019-03-26 12:05:54 -0700

    converted celestial network to dash cytoscape!

--- kmap.py
+++ kmap.py
@@ -1,5 +1,10 @@
 import krpc
 from pprint import pprint
+from dash import Dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_cytoscape as cyto
+
 conn = krpc.connect(name='kpl')
 vessels = []
 for i in conn.space_center.vessels:
@@ -9,7 +14,7 @@ for i in conn.space_center.vessels:
         'radius': i.orbit.radius,
         'mass': i.mass
         })
-pprint(vessels)
+# pprint(vessels)
 
 bodies = []
 for k, b in conn.space_center.bodies.items():
@@ -21,7 +26,19 @@ for k, b in conn.space_center.bodies.items():
     if not b.orbit: body['radius'] = 0
     else: body['radius']  = b.orbit.radius
     bodies.append(body)
-pprint(bodies)
+# pprint(bodies)
 
-# if __name__ == '__main__':
-    # app.run('0.0.0.0', '8888')
+app = Dash()
+style = [{'selector':'node','style':{'content': 'data(label)', 'color':'white'}}]
+app.layout = html.Div([
+    html.H1('kmap'),
+    cyto.Cytoscape(
+        id='cyto',
+        layout={'name': 'circle'},
+        elements=[{'data': { 'id':i['name'], 'label': i['name'] }} for i in bodies],
+        stylesheet=style,
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
