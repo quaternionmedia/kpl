fd50ddde2b4a4a1d9229b003cc1913f5382a0f25
harpo <harpo@harpo.me>
2019-03-26 22:34:44 -0700

    live updating solar map!

--- kmap.py
+++ kmap.py
@@ -1,7 +1,7 @@
 import krpc
 from pprint import pprint
 from dash import Dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_cytoscape as cyto
@@ -10,83 +10,99 @@ from math import log10
 def symlog(n):
     # if n < 0: return -log10(-n)
     # else: return log10(n+1)
-    return n/5000000
+    return n/1000000000
 
 conn = krpc.connect(name='kpl')
-ref = conn.space_center.bodies['Sun'].reference_frame
-
-bodies = []
 body_names = []
-for k, b in conn.space_center.bodies.items():
-    body = {
-        'name': k,
-        'satellites': [s.name for s in b.satellites],
-        'mass': b.mass,
-        'position': b.position(ref),
-        'size': b.equatorial_radius,
-        }
-    if not b.orbit: body['radius'] = 0
-    else: body['radius']  = b.orbit.radius
-    bodies.append(body)
-    body_names.append(k)
-pprint(bodies)
 
-vessels = []
-n = 0
-for i in conn.space_center.vessels:
-    vessels.append({
-        'name': str(n) + i.name,
-        'orbiting': i.orbit.body.name,
-        'radius': i.orbit.radius,
-        'position': i.position(ref),
-        'mass': i.mass
-        })
-    n += 1
-# pprint(vessels)
+def getPositions(n, refName):
+    print('getting positions from', refName)
+    refframe = conn.space_center.bodies[refName].reference_frame
+    bodies = []
+    for k, b in conn.space_center.bodies.items():
+        body = {
+            'name': k,
+            'satellites': [s.name for s in b.satellites],
+            'mass': b.mass,
+            'position': b.position(refframe),
+            'size': b.equatorial_radius,
+            }
+        if not b.orbit: body['radius'] = 0
+        else: body['radius']  = b.orbit.radius
+        bodies.append(body)
+        body_names.append(k)
+    # pprint(bodies)
+
+    vessels = []
+    n = 0
+    for i in conn.space_center.vessels:
+        vessels.append({
+            'name': str(n) + i.name,
+            'orbiting': i.orbit.body.name,
+            'radius': i.orbit.radius,
+            'position': i.position(refframe),
+            'mass': i.mass
+            })
+        n += 1
+    # pprint(vessels)
 
 
-elements = [{'data': {
-                'id':i['name'],
-                'label': i['name']},
-            # 'position':{
-            #     'x': symlog(i['position'][0]),
-            #     'y': symlog(i['position'][1])
-            # },
-            'style': {'size': str(int(i['size']/10000))}} for i in bodies]
-pprint(elements)
+    elements = [ { 'data': {
+                    'id':i['name'],
+                    'label': i['name']},
+                'position':{
+                    'x': 10*symlog(i['position'][0]),
+                    'y': 10*symlog(i['position'][2])
+                },} for i in bodies]
+    # pprint(elements)
 
-for b in bodies:
-    if len(b['satellites']) > 0:
-        for s in b['satellites']:
-            # optional add parents for each satellite
-            # elements[body_names.index(s)]['data']['parent'] = b['name']
-            # add edge from body to satellite
-            elements.append({'data':{'source': b['name'], 'target': s}})
-            print('making edge: ', b['name'], s)
-pprint(elements)
+    for b in bodies:
+        if len(b['satellites']) > 0:
+            for s in b['satellites']:
+                # optional add parents for each satellite
+                # elements[body_names.index(s)]['data']['parent'] = b['name']
+                # add edge from body to satellite
+                elements.append({'data':{'source': b['name'], 'target': s}})
+                # print('making edge: ', b['name'], s)
+    # pprint(elements)
+    return elements
 
 layouts = ['preset', 'grid', 'random', 'circle', 'cose', 'concentric', 'breadthfirst']
 
 
 app = Dash()
-style = [{'selector':'node','style':{'content': 'data(label)', 'color':'white'}}]
+style = [{'selector':'node','style':{'content': 'data(label)', 'color':'white'}}] + [{'selector': i, 'style': {'size': int(i['size']/100)}} for i in body_names]
 app.layout = html.Div(style={'width':'100%', 'height': '100%'}, children=[
     html.H1('kmap'),
     dcc.Dropdown(id='dropdown',
-                value='cose',
+                value='preset',
                 clearable=False,
                 options=[{'label':i, 'value': i} for i in layouts]),
     cyto.Cytoscape(
         id='cyto',
-        layout={'name': 'cose', 'animate' : True, 'animationDuration':1000},
-        elements=elements,
+        layout={'name': 'preset', 'animate' : True, 'animationDuration':1000},
+        elements=getPositions(0, 'Sun'),
         stylesheet=style,
-    )
+    ),
+    dcc.Interval(id='interval-component', interval = 1000, n_intervals=0),
+    dcc.Store(id='storage', storage_type='session', data='Sun'),
 ], )
 
 @app.callback(Output('cyto', 'layout'), [Input('dropdown', 'value')])
 def update_layout(v):
     return {'name': v, 'animate' : True, 'animationDuration':1000}
+
+app.callback(Output('cyto', 'elements'),
+            [Input('interval-component', 'n_intervals')],
+            [State('storage', 'data')])(getPositions)
+
+@app.callback(Output('storage', 'data'), [Input('cyto', 'selectedNodeData')])
+def update_ref(sel):
+    # print('storing ', sel)
+    if not sel: return
+    else:
+        return sel[0]['id']
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
