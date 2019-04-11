060c60ff56026945f3b88ff65f4b666fb8c31f66
harpo <harpo@harpo.me>
2019-04-11 11:02:37 -0700

    html smoothie test using websockets!

--- kerbalapi.py
+++ kerbalapi.py
@@ -6,8 +6,9 @@ from fastapi import FastAPI
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-from starlette.endpoints import WebSocketEndpoint
-
+# from starlette.endpoints import WebSocketEndpoint
+from starlette.websockets import WebSocket
+from asyncio import sleep
 
 
 import uvicorn
@@ -23,7 +24,7 @@ fields = {
 }
 print('connected to', vessel.name)
 
-# pos = conn.add_stream(vessel.position, refframe)
+pos = conn.add_stream(vessel.position, refframe)
 # orbit = conn.add_stream(getattr, vessel.orbit, 'apoapsis')
 
 
@@ -34,23 +35,23 @@ print('connected to', vessel.name)
 app = FastAPI()
 
 app.mount('/deps', StaticFiles(directory='deps'), name='deps')
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory='./')
 
 @app.get('/')
 async def home():
     return HTMLResponse(render_template('kerbal.html'))
 
 @app.websocket_route('/krpc')
-class Krpcws(WebSocketEndpoint):
-    encoding = 'json'
-    async def on_connect(self, websocket):#, data):
-        self.websocket = websocket
-    with conn.stream(vessel.position, refframe) as p:
-        with p.condition:
-            websocket.send_json({'pos': p()})
+async def ws(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        # data = await websocket.receive_text()
+        await websocket.send_json({'pos': pos()})
+        await sleep(.2)
+    await websocket.close()
 
 def render_template(path, **kwargs):
     return templates.get_template(path).render(**kwargs)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=9000, debug=True)
