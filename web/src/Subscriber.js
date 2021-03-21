10184d3c569053cca7fd75a8446d1383784c94e8
Harpo <harpo@harpo.me>
2021-03-20 17:31:01 -0700

    added hack for basic dimension functionaltiy

--- web/src/Subscriber.js
+++ web/src/Subscriber.js
@@ -2,7 +2,7 @@ import m from 'mithril'
 import { Connection } from 'autobahn'
 import { opts } from './Smoothie'
 import { SmoothieChart, TimeSeries } from 'smoothie'
-import { flight_chars } from './constants'
+import { flight_chars, flight_types } from './constants'
 
 var connection = new Connection({
   url: 'ws://' + document.location.host + '/ws',
@@ -18,19 +18,43 @@ connection.open()
 
 export function Subscriber() {
   let chart
-  let series = new TimeSeries
-  
+  let series
+  let dimensions
   return {
     oninit: vnode => {
-      
+      if (vnode.attrs.name in flight_types) {
+        dimensions = flight_types[vnode.attrs.name]
+        series = []
+        for (var i =0; i<dimensions;i++) {
+          series.push(new TimeSeries)
+        }
+      } else {
+        dimensions = 1
+        series = new TimeSeries
+      }
     },
     oncreate: vnode => {
       chart = new SmoothieChart(opts)
-      chart.addTimeSeries(series, { strokeStyle: 'rgba(0, 255, 0, 1)' })
+      if (dimensions > 1) {
+        for (var i =0; i<dimensions;i++) {
+          chart.addTimeSeries(series[i], { strokeStyle: `rgba(${i == 0 ? 255 : 0}, ${i == 1 ? 255 : 0}, ${i == 2 ? 255 : 0}, 1)` })
+        }
+      } else {
+        chart.addTimeSeries(series, { strokeStyle: 'rgba(0, 255, 0, 1)' })
+      }
       connection.session.subscribe('local.ksp.' + vnode.attrs.name, args => {
         // console.log(vnode.attrs.name, args[0])
-        series.append(new Date().getTime(), args[0])
-      })
+        let data = args[0]
+        let t = new Date().getTime()
+        if (typeof(data) == typeof([])) {
+          for (var i =0; i<dimensions;i++) {
+            series[i].append(t, data[i])
+        } 
+      }
+      else {
+        series.append(t, args[0])
+      }
+    })
       chart.streamTo(vnode.dom, 5)
     },
     view: vnode => {
--- web/src/constants.js
+++ web/src/constants.js
@@ -1,1 +1,17 @@
 export const flight_chars = ['aerodynamic_force', 'angle_of_attack',  'atmosphere_density', 'bedrock_altitude', 'center_of_mass', 'direction', 'drag', 'dynamic_pressure', 'elevation', 'equivalent_air_speed', 'g_force', 'heading', 'horizontal_speed', 'latitude', 'lift', 'longitude', 'mach', 'mean_altitude', 'pitch', 'roll', 'rotation', 'sideslip_angle', 'speed', 'speed_of_sound',  'static_air_temperature', 'static_pressure', 'static_pressure_at_msl', 'surface_altitude', 'terminal_velocity',  'total_air_temperature', 'true_air_speed', 'velocity', 'vertical_speed']
+
+export const flight_types = {
+  'aerodynamic_force': 3,
+    'direction': 3,
+    'drag': 3,
+    'lift': 3,
+    'rotation': 4,
+}
+// missing:
+// center_of_mass
+// horizontal_speed
+// roll
+// static_pressure
+// static_pressure_at_msl
+// velocity
+// vertical_speed
