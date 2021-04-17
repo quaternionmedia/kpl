b08e42c9a24a905d287eac10a820e3a4c0cd5f3c
Harpo <harpo@harpo.me>
2021-04-16 17:56:46 -0700

    added config with options

--- kpl/config.py
+++ kpl/config.py
@@ -1,5 +1,8 @@
 from os import environ
 
+STATIC_FILES = environ.get('STATIC_FILES', 'web/static')
+DIST_DIR = environ.get('DIST_DIR', 'web/dist')
+
 KRPC_ADDRESS = environ.get('KRPC_ADDRESS', '127.0.0.1')
 
 KRPC_PORT = 50000
